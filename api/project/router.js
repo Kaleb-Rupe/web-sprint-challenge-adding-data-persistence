ded69bb0aa865adc6f6ad0c41090d650aea4d882
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-09-13 17:55:25 -0400

    Server is up and running.

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,22 @@
 // build your `/api/projects` router here
+const router = require("express").Router();
+const Project = require("./model");
+
+router.get("/:project_id", (req, res, next) => {
+  Project.getProjectById(req.params.project_id)
+    .then((resource) => {
+      res.status(200).json(resource);
+    })
+    .catch(next);
+});
+
+//eslint-disable-next-line
+router.use("*", (err, req, res, next) => {
+  res.status(500).json({
+    customeMessage: "Something went wrong inside the recipes router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,22 @@
 // build your `/api/tasks` router here
+const router = require("express").Router();
+const Task = require("./model");
+
+router.get("/:task_id", (req, res, next) => {
+  Task.getTaskById(req.params.task_id)
+    .then((resource) => {
+      res.status(200).json(resource);
+    })
+    .catch(next);
+});
+
+//eslint-disable-next-line
+router.use("*", (err, req, res, next) => {
+  res.status(500).json({
+    customeMessage: "Something went wrong inside the recipes router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
